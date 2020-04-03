2dfe8ab1fa989e87e4995ff668f81df906171d54
Bhagava <mamidipakabhagavan@gmail.com>
2020-04-03 12:58:17 +0530

    Java_programme

--- java_programmes/src/java_programmes/Prime.java
+++ java_programmes/src/java_programmes/Prime.java
@@ -1,5 +1,25 @@
 package java_programmes;
 
 public class Prime {
-
+	public static void main(String[] args)
+	{
+		int start=20;
+		int end=100;
+		for(int i=start; i<=end; i++)
+		{
+			int count=0;
+			for(int j=2; j<=i/2;j++)
+			{
+				if(i%j==0)
+				{
+					count++;
+					break;
+				}
+			}
+			if(count==0)
+			{
+				System.out.print(i+" ");
+			}
+		}
+	}
 }
